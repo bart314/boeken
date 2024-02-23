143f6eff10859f7c1f08d30ee7758305274c9189
Bart Barnard <b.barnard@pl.hanze.nl>
2024-02-23 15:09:32 +0100

    responsive 💪🧞

--- 2024/scroll.js
+++ 2024/scroll.js
@@ -1,7 +1,12 @@
-const el = document.getElementById('content')
-const data = document.getElementById('sidebar')
-const factor = data.clientHeight / el.clientHeight
+const mq = window.matchMedia('(min-width: 768px)')
+console.log(mq.matches)
 
-document.addEventListener('scroll', () => {
-  data.style.top = 0-factor * document.scrollingElement.scrollTop
-})
+if(mq.matches) {
+  const el = document.getElementById('content')
+  const data = document.getElementById('sidebar')
+  const factor = data.clientHeight / el.clientHeight
+
+  document.addEventListener('scroll', () => {
+    data.style.top = 0-factor * document.scrollingElement.scrollTop
+  })
+}
