673f4f2ae7bfd26ec9d6828236ae2260960ab19c
Bart Barnard <b.barnard@pl.hanze.nl>
2025-03-30 15:32:21 +0200

    Boekje over Bismarcks oorlog en wat verandering in het js

---
+++ js/inject-see-also.js
@@ -0,0 +1,28 @@
+Promise.all([
+    fetch('../js/data/boekendata.json'),
+    fetch('../js/data/recommendations.json')
+])
+.then( resp => Promise.all(resp.map(r => r.json())) )
+.then ( data => {
+    const see_also = document.getElementById('see-also')
+    const boeken = data[0]
+    const recs = data[1].filter(el => el.nr == see_also.dataset['nummer'])
+    if (recs.length > 0) {
+        const api_url = (document.location.href.indexOf('localhost') < 0) 
+            ? 'https://mandarin.nl/boeken/' 
+            : 'http://localhost/~bart/boeken/'
+
+        console.log(api_url)
+
+        let html = '<h2>Bekijk ook eens</h2><ul class="books">'
+        recs[0]['recommendations'].forEach( r => {
+            const book = boeken.filter ( b => b.nr == r)[0]
+            console.log(book)
+            html += `<li><a href="${api_url}${book.link}">`
+            html += `<img src="${api_url}${book.cover}" alt="Cover van het boek">`
+            html += `<p>${book.titel}</p>`
+        })
+        html += '</ul>'
+        see_also.innerHTML = html
+    }
+})
---
+++ test.js
@@ -0,0 +1,6 @@
+const data = [
+    { "nr":65, "recommendations":[60, 54, 30] },
+    { "nr":66, "recommendations":[60, 54, 30] }
+]
+
+console.log(data.filter(el => el.nr == 66))
