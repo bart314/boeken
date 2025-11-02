92af58bdba4c352de810aa0fbdad1980abd2ea56
Bart Barnard <b.barnard@pl.hanze.nl>
2025-11-02 09:40:05 +0100

    Sorteren nu goed.

--- js/see-also.js
+++
@@ -1,7 +0,0 @@
-const els = document.querySelectorAll('#see-also li')
-els.forEach( book => {
-  let html = `<a href="${book.dataset.link}">`
-  html += `<img src="${book.dataset.cover}" alt="Cover van het boek">`
-  html += `<p>${book.dataset.titel}</p>`
-  book.innerHTML = html
-})
