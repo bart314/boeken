61e4414fd5de3d4c8c94c458b3afe0a1f9bf291c
Bart Barnard <b.barnard@pl.hanze.nl>
2025-11-30 11:59:15 +0100

    kleine functionele verbetering.

--- js/hide_summary.js
+++ js/hide_summary.js
@@ -4,8 +4,10 @@ document.querySelector('h2 span').addEventListener('click', el => {
   if (summary.style.display == 'none') { 
     summary.style.display = 'block'
     el.target.innerHTML = '(verbergen)'
+    document.getElementById('word-count').innerHTML = `${summary.dataset['with']} woorden`
   } else {
     summary.style.display = 'none'
     el.target.innerHTML = '(tonen)'
+    document.getElementById('word-count').innerHTML = `${summary.dataset['without']} woorden`
   }
 })
